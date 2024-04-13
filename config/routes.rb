1748aff6e9f13337deafda7cb008cd61f5d1bbec
Jordan Gott <68125689+Trace9723@users.noreply.github.com>
2024-04-12 20:21:48 -0400

    Add files via upload

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root "articles#index"
+
+  resources :articles
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+end
